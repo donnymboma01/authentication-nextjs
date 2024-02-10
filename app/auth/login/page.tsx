1e1f8e4a3ede0583d525ddedd2012d3dc7d5dddf
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-02-10 22:19:40 +0100

    auth : login form & page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/auth/Login-form"; 
+
+const LoginPage = () => {
+  return (
+    <LoginForm />
+  );
+};
+
+export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
-
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800 " >
-      <div className="space-y-6 text-center" >
-        <h1 className="text-6xl font-semibold text-white drop-shadow-md" > 🔐 Auth</h1>
-        <p className="text-white text-lg" >A simple authentication service</p>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800 ">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          {" "}
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          Un simple service d'authentification
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Se connecter
+            </Button>
+          </LoginButton>
+        </div>
       </div>
     </main>
   );
