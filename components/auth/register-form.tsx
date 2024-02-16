1c8e5b2555732bb2ff3a478451791f05fbeb5fba
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-02-16 16:25:23 +0100

    feat: prisma setup

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { useState, useTransition } from "react";
+
+import { Input } from "../ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { CardWrapper } from "./card-wrapper";
+import { RegisterSchema } from "@/schemas";
+import { Button } from "../ui/button";
+import { FormError } from "../form-errors";
+import { FormSuccess } from "../form-success";
+import { register } from "@/actions/register";
+
+export const RegisterForm = () => {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    // axios.post("/api/auth/login", values); si jamais on utilise une API externe.
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      register(values).then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Créer un compte"
+      backButtonLabel="Vous avez déjà un compte ?"
+      backButtonHref="/auth/login"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      {...field}
+                      type="email"
+                      placeholder="johndoe@gmail.com"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Mot de passe</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      {...field}
+                      type="password"
+                      placeholder="********"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nom</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isPending}
+                      {...field}
+                      placeholder="John Doe"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+
+          <Button disabled={isPending} type="submit" className="w-full">
+            Inscription
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     message: "Veuillez entrer un mot de passe valide",
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Veuillez entrer une adresse email valide",
+  }),
+  password: z.string().min(5, {
+    message: "Minimum 5 caractères requis",
+  }),
+  name: z.string().min(5, { 
+    message: "Le champ nom est obligatoire",
+  }),
+});
