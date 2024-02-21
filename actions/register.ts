95c5ba8f0a31a8653b0ba787c8f7bb0b87268dc0
donnymboma01 <82469294+donnymboma01@users.noreply.github.com>
2024-02-21 20:47:28 +0100

    feat: user registration is working

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,40 @@
 "use server";
 
-import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  // console.log(values);
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Email sent" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // const user_exists = await db.user.findUnique({
+  //   where: { email },
+  // });
+  // alternative de bcrypt : bcryptjs
+  const user_exists = await getUserByEmail(email);
+
+  if (user_exists) {
+    return { error: "Cet utilisateur existe déjà" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Envoi d'un email de confirmation.
+  return { success: "Utilisateur créé avec succes" };
 };
